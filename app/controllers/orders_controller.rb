cdc352d02b2e6e648179387fa1409740a817f8bb
Jordan Gilbert <jordan.gilbert@hotmail.co.uk>
2025-05-20 15:09:04 +0100

    orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,25 @@
 class OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to @order, notice: 'Order was successfully created.'
+    else
+      render :new
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:power_up_id, :quantity, :user_id, :accepted, :total_price, :order_date)
+  end
+
 end
