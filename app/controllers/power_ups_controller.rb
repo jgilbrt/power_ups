686598a0e40a3bca3d86086bf89925f9d4ae3170
Jordan Gilbert <jordan.gilbert@hotmail.co.uk>
2025-05-22 12:45:47 +0100

    index filtering + updated powerup controller + sells category form

--- app/controllers/power_ups_controller.rb
+++ app/controllers/power_ups_controller.rb
@@ -3,7 +3,21 @@ class PowerUpsController < ApplicationController
 
   def index
     @powerUps = PowerUp.all
+
+    @powerUps = PowerUp.left_outer_joins(:orders).where(orders: { accepted: [false, nil] }).distinct
+      if params[:category].present? && params[:category] != "All"
+        @powerUps = @powerUps.where(category: params[:category])
+      end
+
+      if params[:min_price].present?
+        @powerUps = @powerUps.where("price >= ?", params[:min_price])
+      end
+
+      if params[:max_price].present?
+        @powerUps = @powerUps.where("price <= ?", params[:max_price])
+      end
   end
+
 
   def show
     @powerUp = PowerUp.find(params[:id])
@@ -11,6 +25,7 @@ class PowerUpsController < ApplicationController
 
   def new
     @powerUp = PowerUp.new
+    @categories = PowerUp.distinct.pluck(:category).compact
   end
 
   def create
