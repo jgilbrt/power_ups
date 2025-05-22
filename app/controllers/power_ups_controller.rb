99d0756720f2db83e9d273f184042067318e951d
Jordan Gilbert <jordan.gilbert@hotmail.co.uk>
2025-05-22 16:38:38 +0100

    added delete option to lisitngs , changed controllers

--- app/controllers/power_ups_controller.rb
+++ app/controllers/power_ups_controller.rb
@@ -43,6 +43,12 @@ class PowerUpsController < ApplicationController
     end
   end
 
+  def destroy
+    power_up = PowerUp.find(params[:id])
+    power_up.destroy
+    redirect_to listings_path, notice: "Listing removed successfully."
+  end
+
   private
 
   def powerUp_params
