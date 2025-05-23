0b4b4c721effa424c9d750c10bbe867bfe39eeb1
Alez05 <cercel.alexandru436@gmail.com>
2025-05-23 12:14:08 +0100

    Use DATABASE_URL for production db

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get '/signup', to: redirect('/users/sign_up')
+  get '/login', to: redirect('/users/sign_in')
+
 end
